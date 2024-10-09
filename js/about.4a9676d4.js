be9b15afa003db30a2fede6d917184762a8559c8
Kenny <56199201+zzlweb@users.noreply.github.com>
2024-10-09 17:40:10 +0800

    Add files via upload

---
+++ js/about.4a9676d4.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdemo"]=self["webpackChunkdemo"]||[]).push([[594],{603:function(n,u,e){e.r(u),e.d(u,{default:function(){return i}});var t=e(6768);const a={class:"about"};function r(n,u){return(0,t.uX)(),(0,t.CE)("div",a,u[0]||(u[0]=[(0,t.Lk)("h1",null,"This is an about page",-1)]))}var s=e(1241);const c={},o=(0,s.A)(c,[["render",r]]);var i=o}}]);
+//# sourceMappingURL=about.4a9676d4.js.map
